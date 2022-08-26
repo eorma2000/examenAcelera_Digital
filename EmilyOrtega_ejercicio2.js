c82e4b1ea73fca55390801bcb46dbd149bc142f4
eorma2000 <110936444+eorma2000@users.noreply.github.com>
2022-08-25 23:35:59 -0500

    Add files via upload
    examen terminado

---
+++ EmilyOrtega_ejercicio2.js
@@ -0,0 +1,10 @@
+/*CICLOS*/
+
+/*Muestra los números del 660 al 360, contando de 20 en 20 hacia atrás utilizando un bucle
+for. Guardar archivo con el nombre TuNombre_Examen2*/
+
+
+for(let pasos=660; pasos<5; pasos++){
+    if(pasos===361){break;}
+    console.log(pasos);
+}
